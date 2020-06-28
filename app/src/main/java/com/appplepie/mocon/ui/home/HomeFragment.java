439b6d9c7ee31810b3dc8f53f156def5e3a8d059
moseori <haojile96@gmail.com>
2020-06-28 17:29:09 +0900

    AppTheme : noActionBar

--- app/src/main/java/com/appplepie/mocon/ui/home/HomeFragment.java
+++ app/src/main/java/com/appplepie/mocon/ui/home/HomeFragment.java
@@ -16,12 +16,9 @@ import com.appplepie.mocon.R;
 
 public class HomeFragment extends Fragment {
 
-    private HomeViewModel homeViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
         return root;
     }
